8a963f39f86e2053a51f234c663285904788f6c4
WhyRedX <thapabhupendra9779@gmail.com>
2024-03-06 20:06:08 +0530

    backend commit

---
+++ controller/userController.js
@@ -0,0 +1,66 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+// Register a new user
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, mobile, password } = req.body; // extracting the data from the request body
+
+  const userExitsEmail = await User.findOne({ email }); // checking if the user already exists
+  const userExitsMobile = await User.findOne({ mobile }); // checking if the user already exists
+
+  if (userExitsEmail || userExitsMobile) {
+    // if the user already exists
+    res.status(400);
+    throw new Error("User already exists"); // throwing an error
+  }
+
+  const user = await User.create({ name, email, mobile, password }); // if not exits then creating a new user
+
+  if (user) {
+    res.json({
+      message: "User created successfully", // sending a message in console
+    });
+  } else {
+    res.status(400); // if not created then sending a status code
+    throw new Error("Error Occured"); // throwing an error
+  }
+});
+
+// authenticate user
+
+const authUser = asyncHandler(async (req, res) => {
+  // authenticating a user
+  const { email, password } = req.body; /// extracting the data from the request body
+
+  const user = await User.findOne({ email }); // finding the user
+
+  if (user && (await user.matchPassword(password))) {
+
+    token = await user.generateAuthToken(); // generating a token
+    console.log(token);
+
+    res.cookie("loginToken", token,{
+      expires: new Date(Date.now() + 25892000000),
+      httpOnly: true,
+    });
+
+    res.json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      mobile: user.mobile,
+      token: token,});
+
+  } else {
+    res.status(400);
+    throw new Error("Invalid Email or Password"); // throwing an error
+  }
+});
+
+const logoutUser = async (req, res) => {
+  res.clearCookie("loginToken", {path: "/"});
+  res.status(200).send("user logout");  
+}
+
+module.exports = { registerUser, authUser, logoutUser } // exporting the functions
---
+++ server.js
@@ -0,0 +1,35 @@
+const shopCards = require("./data/shop_cards"); // Import shop cards
+const showCaseCards = require("./data/showcase_cards"); // Import showcase cards
+
+// Creating a server
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db"); // Import connectDB for connecting to MongoDB
+const userRoutes = require("./routes/userRoutes");
+// const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+
+dotenv.config(); // Configure dotenv
+connectDB(); // Connect to MongoDB
+
+app.use(express.json()); // Configure express to parse JSON
+
+app.get("/api/shopCards", (req, res) => {
+  // Get shop cards
+  res.json(shopCards); // Send shop cards
+});
+
+app.get("/api/showCaseCards", (req, res) => {
+  // Get showcase cards
+  res.json(showCaseCards); // Send showcase cards
+});
+
+app.use("/api/users", userRoutes); // Use user routes
+
+
+// app.use(notFound); // Use notFound middleware
+// app.use(errorHandler); // Use errorHandler middleware)
+
+const PORT = process.env.PORT || 8000; // Configure port
+
+app.listen(PORT, console.log(`server runninng on port ${PORT}`)); // Start server
